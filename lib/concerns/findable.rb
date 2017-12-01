82ace521668fd544e18c56b6e455682c19d1e2d2
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-12-01 19:18:14 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,9 +1,9 @@
 module Concerns::Findable
-  def find_by_name(name)
+  def find_by_name(names)
     all.detect{ |o| o.name == name }
   end
 
-  def find_or_create_by_name(name)
+  def find_or_create_by_name(names)
     find_by_name(name) || create(name)
   end
 end
